869c2b46ce612294341a06cde9c01cb5cf1f2200
Tony <tonydanilofroes@hotmail.com>
2019-04-06 16:13:00 -0300

    Python

--- Lista 1 - Python/Exec1.py
+++ Lista 1 - Python/Exec1.py
@@ -1,1 +1,9 @@
-Valor = input("teste")
+#coding: utf8
+
+N1 = float(input("Valor: \n"))
+N2 = float(input("Valor 2: \n"))
+
+R = float((N1 + N2) / int(2))
+
+
+print ("A média é:",R)
--- Lista 1 - Python/Exec2.py
+++ Lista 1 - Python/Exec2.py
@@ -1,1 +1,1 @@
-teste
+utf
