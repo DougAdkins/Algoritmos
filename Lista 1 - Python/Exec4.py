dd55897e75572ee112506efe8bdb94bf4bfbe802
DougAdkins <tonydanilofroes@hotmail.com>
2019-04-17 14:16:34 -0300

    Python

---
+++ Lista 1 - Python/Exec4.py
@@ -0,0 +1,11 @@
+#coding: utf8
+
+Q1 = float(input("Digite q1: "))
+Q2 = float(input("Digite q2: "))
+D = int(input("Digite a distância: "))
+
+k = 9*10e9
+d = (D*2)
+F = float(k*((Q1*Q2)/d))
+
+print (F)
---
+++ Lista 1 - Python/Exec5.py
@@ -0,0 +1,13 @@
+#coding: utf8
+
+V1 = float(input("Digite um primeiro valor: ")) 
+V2 = float(input("Digite um segundo valor: "))
+
+C = V1
+V1 = V2
+V2 = C
+
+
+
+print ("O  primeiro valor é", V1)
+print ("O segundo valor é",C)
