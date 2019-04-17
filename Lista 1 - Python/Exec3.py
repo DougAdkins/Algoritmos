42a73c34b21119eb8c44dd27f2ab5f69ef9e838b
DougAdkins <tonydanilofroes@hotmail.com>
2019-04-17 13:55:14 -0300

    Python

--- Lista 1 - Python/Exec3.py
+++ Lista 1 - Python/Exec3.py
@@ -1,1 +1,13 @@
-test()
+#coding: utf8
+
+P1 = int(input("Digite o primeiro peso:"))
+P2 = int(input("Digite o segundo peso:"))
+P3 = int(input("Digite o terceiro peso:"))
+
+N1 = float(input("Digite a primeira nota: "))
+N2 = float(input("Digite a segunda nota: "))
+N3 = float(input("Digite a terceira nota: "))
+
+R = ((N1*P1)+(N2*P2)+(N3*P3))/(P1+P2+P3)
+
+print ("A média é: ", R)
