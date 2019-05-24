737fbcab0c3d79c6f064634793887412ce34e5ca
Tony Froes <42457749+DougAdkins@users.noreply.github.com>
2019-05-24 14:57:21 -0300

    Add files via upload

---
+++ Lista 1 - Python/error.py
@@ -0,0 +1,26 @@
+
+
+def maior_elemento(maior_numero):
+	maior_numero = max(numero)
+	print (maior_numero)
+
+def media_elemento(media_numero):
+	media_numero = sum(numero) / cont 
+	print (media_numero)
+
+cont = 0
+numero = []
+while True:
+	outronome = float(input("Digite um numero: "))
+	if outronome == 0:
+		break
+	numero.append(outronome)
+	cont += 1
+	print (numero)
+
+maior_elemento(numero)
+media_elemento(numero)
+
+
+
+	
